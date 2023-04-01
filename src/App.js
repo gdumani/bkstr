49dc0748aad0d0fcccfbffda55bd4a718b37ff26
gdumani <gdumani@gmail.com>
2023-04-01 14:18:48 -0600

    add booklist and book components Books landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
 
 const App = () => (
   <BrowserRouter>
