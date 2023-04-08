a9047eafa40e7f2ea98fd9c50113e2f7b2c523e2
gdumani <gdumani@gmail.com>
2023-04-08 09:58:17 -0600

    install axios and add logic to retrieve API books

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,6 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API_ID = 'FN35ZgxeHr1ORapGK8sX';
+const API_URL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${API_ID}/books/`;
 
 const initialState = [];
+
+export const getBooks = createAsyncThunk('books/getBooks', async () => {
+  const response = await axios.get(API_URL).then(
+    (apiBooks) => Object.entries(apiBooks.data).map(([id, bk]) => ({ id, ...bk[0] })),
+  );
+  return response;
+});
 
 const booksSlice = createSlice({
   name: 'books',
@@ -8,14 +19,18 @@ const booksSlice = createSlice({
   reducers: {
     addBook: (state, action) => {
       const newBook = {
-        item_id: action.payload.id,
+        id: action.payload.id,
         title: action.payload.title,
         author: action.payload.author,
         category: action.payload.category,
       };
       return [...state, newBook];
     },
-    removeBook: (state, action) => [...state].filter((book) => book.item_id !== action.payload),
+    removeBook: (state, action) => [...state].filter((book) => book.id !== action.payload),
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(getBooks.fulfilled, (state, action) => action.payload);
   },
 });
 
